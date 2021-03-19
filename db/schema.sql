5dcd5ed0ddf4999889f4df3ba6c3b35a1fe41854
Michael Kolarik <mikekolarik@gmail.com>
2021-03-19 14:37:29 -0500

    Update schema for docker

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,23 @@ CREATE DATABASE questions_and_answers;
 USE questions_and_answers;
 
 CREATE TABLE questions (
-  id INT NOT NULL AUTO_INCREMENT,
+  question_id INT NOT NULL AUTO_INCREMENT,
   product_id INT NOT NULL,
-  body VARCHAR(1000) NOT NULL,
-  date_added DATE NOT NULL,
-  name VARCHAR(60) NOT NULL,
+  question_body VARCHAR(1000) NOT NULL,
+  question_date DATE NOT NULL,
+  asker_name VARCHAR(60) NOT NULL,
   email VARCHAR(60) NOT NULL,
   reported BOOLEAN NOT NULL DEFAULT 0,
-  helpfulness INT NOT NULL DEFAULT 0,
+  question_helpfulness INT NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE answers (
-  id INT NOT NULL AUTO_INCREMENT,
+  answer_id INT NOT NULL AUTO_INCREMENT,
   question_id INT NOT NULL,
   body VARCHAR(1000) NOT NULL,
-  date_added DATE NOT NULL,
-  name VARCHAR(60) NOT NULL,
+  date DATE NOT NULL,
+  answerer_name VARCHAR(60) NOT NULL,
   email VARCHAR(60) NOT NULL,
   reported BOOLEAN NOT NULL DEFAULT 0,
   helpfulness INT NOT NULL DEFAULT 0,
