cc16a62be994b78822d1aca6194f2d58592ec356
Michael Kolarik <mikekolarik@gmail.com>
2021-03-19 16:56:52 -0500

    Add console log to db index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2");
 require("dotenv").config("../.env");
 
+console.log(process.env.DB_USER);
+
 const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
